c27f09c00761699401edde8a921a01bb64332cb9
Administrator <kh-a-r@yandex.ru>
2020-12-09 23:17:55 +0500

    9.1. String [#425888]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,7 +2,9 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        double rslt = p / (2 * (k + 1)) * p / (2 * (k + 1)) * k;
+        double side1 = p / (2 * (k + 1));
+        double side2 = p / (2 * (k + 1)) * k;
+        double rslt =  side1 * side2;
         return rslt;
     }
 
