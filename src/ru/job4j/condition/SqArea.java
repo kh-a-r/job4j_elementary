f5c7da6f0933ba4ed002cc334e0fb5e74863b0e7
Administrator <kh-a-r@yandex.ru>
2020-12-14 18:09:20 +0500

    4.6.1. Switch [#425834]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -4,7 +4,7 @@ public class SqArea {
     public static double square(int p, int k) {
         double side1 = p / (2 * (k + 1));
         double side2 = side1 * k;
-        double rslt =  side1 * side2;
+        double rslt =  side1 + side2;
         return rslt;
     }
 
--- test/ru/job4j/condition/MultiMaxTest.java
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -9,25 +9,25 @@ public class MultiMaxTest {
 
     @Test
     public void whenMaxFirst() {
-        int rsl = MultiMax.max(6,3,4);
+        int rsl = MultiMax.max(6, 3, 4);
         Assert.assertTrue(true);
     }
 
     @Test
     public void whenMaxSecond() {
-        int rsl = MultiMax.max(5,8,4);
+        int rsl = MultiMax.max(5, 8, 4);
         Assert.assertTrue(true);
     }
 
     @Test
     public void whenMaxThird() {
-        int rsl = MultiMax.max(2,7,9);
+        int rsl = MultiMax.max(2, 7, 9);
         Assert.assertTrue(true);
     }
 
     @Test
     public void whenMaxAll() {
-        int rsl = MultiMax.max(4,4,4);
+        int rsl = MultiMax.max(4, 4, 4);
         Assert.assertTrue(true);
     }
 
