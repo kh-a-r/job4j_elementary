fe08d41509d5d522fc8b09103dd391b300ed90a7
Administrator <kh-a-r@yandex.ru>
2020-12-16 23:56:05 +0500

    5.9.1. Генерация конструкций в IDEA [#425942]

---
+++ src/ru/job4j/loop/Loop.java
@@ -0,0 +1,9 @@
+package ru.job4j.loop;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < ; i++) {
+            System.out.println();
+        }
+    }
+}
