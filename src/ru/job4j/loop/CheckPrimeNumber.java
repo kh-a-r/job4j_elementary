3ff3a5c98833d451fc29910913e18ea2238d690a
Administrator <kh-a-r@yandex.ru>
2020-12-17 16:31:26 +0500

    6.0.1. Объявление массива. [#425874]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int n) {
         boolean prime = true;
-        for (int i = 2; i <= n - 1; i++) {
+        for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 prime = false;
                 break;
--- src/ru/job4j/loop/Loop.java
+++ src/ru/job4j/loop/Loop.java
@@ -1,9 +1,14 @@
 package ru.job4j.loop;
 
 public class Loop {
-    public static void main(String[] args) {
-        for (int i = 0; i < ; i++) {
-            System.out.println();
+    public static void out(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
         }
+        return;
+    }
+
+    public static void main(String[] args) {
+        Loop.out(6);
     }
 }
