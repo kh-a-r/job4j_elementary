041f386927c664e0780c0bc19227bfcacc433451
Administrator <kh-a-r@yandex.ru>
2020-12-28 00:15:06 +0500

    1.7. Конструктор [#426045]

---
+++ src/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String furniture) {
+        furniture = desc;
+    }
+
+    public static void main(String[] args) {
+        Fix item = new Fix("Fix me ASAP.");
+    }
+}
