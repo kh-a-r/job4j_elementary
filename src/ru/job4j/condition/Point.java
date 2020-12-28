9e9e1f25342fcf11db0b8b5f86e8027614840abb
Administrator <kh-a-r@yandex.ru>
2020-12-28 17:18:30 +0500

    2.1. Рефакторинг - Расстояние между точками. [#426034]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,13 +1,26 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int x2, int y1, int y2) {
-        double rslt = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        return rslt;
+
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow((this.x - that.x), 2) + pow((this.y - that.y), 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(11, 33, 11, 55);
+        Point a = new Point(1, 1);
+        Point b = new Point(3, 5);
+        double result = a.distance(b);
         System.out.println(result);
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -7,47 +7,28 @@ import static org.junit.Assert.assertEquals;
 
 public class PointTest {
 
+
     @Test
     public void whenA1B3C1D5Then4() {
-        int a = 1;
-        int b = 3;
-        int c = 1;
-        int d = 5;
+        Point a = new Point(1, 1);
+        Point b = new Point(3, 5);
+        double rsl = a.distance(b);
         double expected = 4.47;
-        double out = Point.distance(a, b, c, d);
-        assertEquals(expected, out, 0.1);
-    }
 
-    @Test
-    public void whenA2B3C4D5Then1() {
-        int a = 2;
-        int b = 3;
-        int c = 4;
-        int d = 5;
-        double expected = 1.41;
-        double out = Point.distance(a, b, c, d);
-        assertEquals(expected, out, 0.1);
-    }
-
-    @Test
-    public void whenA11B33C11D55Then49() {
-        int a = 11;
-        int b = 33;
-        int c = 11;
-        int d = 55;
-        double expected = 49.19;
-        double out = Point.distance(a, b, c, d);
-        assertEquals(expected, out, 0.1);
+    /* int a = 1;
+     int b = 3;
+     int c = 1;
+     int d = 5;
+     double out = Point.distance(a, b, c, d);*/
+    assertEquals(expected, rsl, 0.01);
     }
 
     @Test
     public void whenA0B1C1D1Then1() {
-        int a = 0;
-        int b = -1;
-        int c = 0;
-        int d = -1;
+        Point a = new Point(0, 0);
+        Point b = new Point(-1, -1);
+        double rsl = a.distance(b);
         double expected = 1.41;
-        double out = Point.distance(a, b, c, d);
-        assertEquals(expected, out, 0.1);
+        assertEquals(expected, rsl, 0.1);
     }
 }
