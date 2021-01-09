c7030d7d1874381b7b7335c0a7f613e17049679d
Administrator <kh-a-r@yandex.ru>
2021-01-10 00:57:45 +0500

    3.2. Удаление моделей из массива. [#426065]

--- src/ru/job4j/pojo/ShopDrop.java
+++ src/ru/job4j/pojo/ShopDrop.java
@@ -3,17 +3,16 @@ package ru.job4j.pojo;
 public class ShopDrop {
     public static Product[] leftShift(Product[] products, int index) {
         for (int i = index; i < products.length - 1; i++) {
-           Product temp = products[i];
             products[i] = products[i + 1];
-            products[i + 1] = temp;
         }
+        products[products.length - 1] = null;
         return products;
     }
 
     public static void main(String[] args) {
         Product[] products = new Product[4];
-        products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
+        products[0] = new Product("Oil", 10);
+        products[1] = new Product("Salt", 4);
         products[2] = new Product("Egg", 19);
         products[3] = new Product("Oil", 19);
 
@@ -22,23 +21,13 @@ public class ShopDrop {
         }
 
         System.out.println();
-       products[1] = null;
 
-       for (int i = 0; i < products.length; i++) {
-           if (products[i] != null) {
-               System.out.print(products[i].getName() + " ");
-           } else {
-               System.out.print("null" + " ");
-           }
-       }
-        System.out.println();
-
-        Product[] prod = leftShift(products, 1);
+        Product[] prod = leftShift(products, 0);
         for (int j = 0; j < prod.length; j++) {
                 if (prod[j] != null) {
-                    System.out.println(prod[j].getName());
+                    System.out.print(prod[j].getName() + " ");
                 } else {
-                    System.out.println("null");
+                    System.out.print("null");
                 }
             }
         }
