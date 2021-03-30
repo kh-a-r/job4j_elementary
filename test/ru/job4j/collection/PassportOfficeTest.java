a2bc4abdc392266af68e3a1ae5ec38ceb57f92ca
Administrator <kh-a-r@yandex.ru>
2021-03-30 15:51:17 +0500

    2. Преобразования List в Map. [#425977]

--- test/ru/job4j/collection/PassportOfficeTest.java
+++ test/ru/job4j/collection/PassportOfficeTest.java
@@ -2,6 +2,9 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -12,6 +15,17 @@ public class PassportOfficeTest {
         Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
         PassportOffice office = new PassportOffice();
         office.add(citizen);
-        assertThat(office.get (citizen.getPassport()), is(citizen));
+        assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void addDublicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizenDubl = new Citizen("2f44a", "Ivan Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizenDubl);
+        assertThat(office.get(citizenDubl.getPassport()).getUsername(), is(citizen.getUsername()));
+    }
+
 }
