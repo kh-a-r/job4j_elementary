3bd9542ff53266022c07b9c1c3e1ca97c5123d0d
Administrator <kh-a-r@yandex.ru>
2021-05-08 20:40:51 +0500

    Проверить две строчки на идентичность.

--- src/main/java/ru/job4j/collection/NonUniqueString.java
+++ src/main/java/ru/job4j/collection/NonUniqueString.java
@@ -4,16 +4,54 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NonUniqueString {
-    public static Map<String, Boolean> checkData(String[] strings) {
-        Map<String, Boolean> mapList = new HashMap<>();
-        for (String element : strings
-        ) {
-            if (!mapList.containsKey(element)) {
-                mapList.put(element, Boolean.FALSE);
+    /**
+     * Собираем в карту - Map<Character, Integer> - ключ - символ, значение - сколько этот символ раз встречается в слове
+     * @param string строка, которую разберем для карты
+     * @return возвращает карту
+     */
+
+    public static Map<Character, Integer> map(String string) {
+        Map<Character, Integer> mapList = new HashMap<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (!mapList.containsKey(string.charAt(i))) {
+                mapList.put(string.charAt(i), 1);
             } else {
-                mapList.put(element, Boolean.TRUE);
+                int v = mapList.get(string.charAt(i));
+                v++;
+                mapList.put(string.charAt(i), v);
             }
-            }
+        }
         return mapList;
+    }
+
+    /**
+     *  Метод перебирает второе слово и  сверяет его с картой
+     *       - если символа такого вообще нет - сразу вернуть false
+     *       - если символ есть и ассоциированное с ним значение равно 1 -  удалить такую пару
+     *       - если символ есть и ассоциированное с ним значение больше 1 - уменьшаем значение на 1
+     *      Если после таких манипуляций окажется что карта пустая - значит слова одинаковые.
+     * @param left исходная строка
+     * @param right сравниваемая строка
+     * @return возвращает false, если хоть один символ отсутствует , true - если карту пустая
+     */
+    public static boolean eq(String left, String right) {
+        boolean rsl = true;
+        Map<Character, Integer> list = map(left);
+        for (int i = 0; i < right.length(); i++) {
+            if (!list.containsKey(right.charAt(i))) {
+                rsl = false;
+                break;
+            }
+            if (list.containsKey(right.charAt(i)) && list.get(right.charAt(i)) == 1) {
+                list.remove(right.charAt(i));
+            } else {
+                int v = list.get(right.charAt(i));
+                v--;
+            }
+        }
+        if (list.isEmpty()) {
+            rsl = true;
+        }
+        return rsl;
     }
 }
---
+++ test/ru/job4j/collection/NonUniqueStringTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class NonUniqueStringTest {
+
+    @Test
+    public void whenEq() {
+        assertThat(NonUniqueString.eq("Hello", "Hlloe"), is(true));
+    }
+
+    @Test
+    public void whenNotEq() {
+        assertThat(NonUniqueString.eq("Hello", "Halle"), is(false));
+    }
+
+    @Test
+    public void whenNotMultiEq() {
+        assertThat(NonUniqueString.eq("heloo", "hello"), is(false));
+    }
+}
