fcd69a2ff42454aa28f5573101b002f5006beec8
Administrator <kh-a-r@yandex.ru>
2021-06-03 22:49:52 +0500

    3. Optional в банковских переводах. [#214711]

---
+++ src/main/java/ru/job4j/map/College.java
@@ -0,0 +1,53 @@
+package ru.job4j.map;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.Assert.assertThat;
+
+public class College {
+    private final Map<Student, Set<Subject>> students;
+
+    public College(Map<Student, Set<Subject>> students) {
+        this.students = students;
+    }
+
+    public Optional<Student> findByAccount(String account) {
+        Optional<Student> rsl = Optional.empty();
+        for (Student s: students.keySet()) {
+            if (account.equals(s.getAccount())) {
+                rsl = Optional.of(s);
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Subject> rsl = Optional.empty();
+        Optional<Student> s = findByAccount(account);
+        if (s.isPresent()) {
+            Set<Subject> subjects = students.get(s);
+            for (Subject subj : subjects) {
+                if (name.equals(subj.getName())) {
+                    rsl = Optional.of(subj);
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                )
+        );
+        College college = new College(students);
+        System.out.println(college.findBySubjectName("000001", "Sociology"));
+    }
+}
